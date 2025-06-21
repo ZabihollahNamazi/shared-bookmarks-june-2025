44ed336d42f7caa3e9795fad91b92b9fc675051b
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-06-21 12:52:47 +0100

    data form added for UI

--- script.js
+++ script.js
@@ -4,9 +4,10 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getUserIds } from "./storage.js";
+import { getUserIds, setData, getData, clearData } from "./storage.js";
 
 function userDropdown(users){
+  const userForm = document.getElementById("user-form");
   const userDropDown = document.getElementById("user-dropDown");
 
   for(let i = 0; i < users.length; i++){
@@ -15,9 +16,39 @@ function userDropdown(users){
     option.innerHTML = `User ${users[i]}`;
     userDropDown.appendChild(option);
   }
+
+  userForm.addEventListener("change", ()=>{
+    const userDropDown = document.getElementById("user-dropDown");
+    const selectedUser = userDropDown.options[userDropDown.selectedIndex].value;
+    console.log(selectedUser);
+    clearData(selectedUser)
+    displayData(selectedUser);
+  })
 }
 
+function addDataToStorage(){
+  
 
+
+}
+
+function displayData(userId){
+  const bookmarkList = document.getElementById("bookmarks-list");
+  bookmarkList.innerHTML = "";
+
+  const receivedUserData = getData(userId);
+  console.log(receivedUserData);
+  if(!receivedUserData){
+    let li = document.createElement("li");
+    li.innerHTML = "There is no bookmarks for this user";
+    bookmarkList.appendChild(li);
+  }
+  else{
+    let lii = document.createElement("li");
+    lii.innerHTML = receivedUserData;
+    bookmarkList.appendChild(lii);
+  }
+}
 
 
 window.onload = function () {
