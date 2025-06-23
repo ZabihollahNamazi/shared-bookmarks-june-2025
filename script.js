d062d9177c29c1db2a002c2a0795f628c7d5bcaa
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-06-23 22:25:10 +0100

    removed datedisplay function from script.js

--- script.js
+++ script.js
@@ -103,13 +103,6 @@ function displayData(userId){
   }
 }
 
-export function dateForDisplay(dateObj){
-      const year = dateObj.getFullYear();
-      const month = String(dateObj.getMonth() + 1).padStart(2, '0'); // add 1 & pad with zero
-      const day = String(dateObj.getDate()).padStart(2, '0');
-
-      return `${year}-${month}-${day}`;
-}
 
 
 window.onload = function () {
